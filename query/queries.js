1e185c477cefc8fcbcc184cb7a4e4389df7b2609
Adrià Quiñoa Jimenez <adriaquinoajimenez@gmail.com>
2026-02-10 12:57:16 +0100

    Update queries.js

--- query/queries.js
+++ query/queries.js
@@ -2,7 +2,7 @@
 db.restaurants.find({}, {_id: 0})
 
 // 2. Mostrar el restaurant_id i name per tots els documents en la col·lecció Restaurants.
-
+db.restaurants.find({}, {name: 1, restaurant_id: 1, _id: 0})
 
 // 3. Mostrar el restaurant_id, name, borough i cuisine.
 
