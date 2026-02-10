105da36a3484fde846953be4b082187b040d9bf8
Adrià Quiñoa Jimenez <adriaquinoajimenez@gmail.com>
2026-02-10 13:04:23 +0100

    Update queries.js

--- query/queries.js
+++ query/queries.js
@@ -5,13 +5,13 @@ db.restaurants.find({}, {_id: 0})
 db.restaurants.find({}, {name: 1, restaurant_id: 1, _id: 0})
 
 // 3. Mostrar el restaurant_id, name, borough i cuisine.
-
+db.restaurants.find({}, {name: 1, restaurant_id: 1, borough: 1, cuisine: 1, _id: 0})
 
 // 4. Mostrar restaurant_id, name, borough i zip code.
-
+db.restaurants.find({}, {name: 1, restaurant_id: 1, borough: 1, "address.zipcode": 1, _id: 0})
 
 // 5. Mostrar tots els restaurants que estan en el Bronx.
-
+db.restaurants.find({borough: "Bronx"}, {_id: 0})
 
 // 6. Mostrar els primers 5 restaurants que estan en el Bronx.
 
