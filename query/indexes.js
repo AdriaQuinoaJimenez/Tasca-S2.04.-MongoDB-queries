f195108de2aa57bb6da0690234737c8b148c03d4
Adrià Quiñoa Jimenez <adriaquinoajimenez@gmail.com>
2026-02-11 15:32:54 +0100

    Update indexes.js

--- query/indexes.js
+++ query/indexes.js
@@ -2,3 +2,4 @@
 db.restaurants.createIndex({ name: 1 });
 db.restaurants.createIndex({ borough: 1 });
 db.restaurants.createIndex({ "grades.score": 1 });
+db.restaurants.createIndex({ "location.coordinates.0": 1 });
