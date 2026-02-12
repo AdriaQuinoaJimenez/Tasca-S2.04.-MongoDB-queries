77099997a1ed10f5f4549019cd270d4948447b6d
Adrià Quiñoa Jimenez <adriaquinoajimenez@gmail.com>
2026-02-12 12:03:23 +0100

    Update indexes.js

--- query/indexes.js
+++ query/indexes.js
@@ -8,3 +8,4 @@ db.restaurants.createIndex( {cuisine: -1}, { name: "filter_by_grade", partialFil
 db.restaurants.createIndex( {name: 1}, { name: "filtered_by_wil", partialFilterExpression: {name: {$gte: "Wil", $lte: "Wim"}}});
 db.restaurants.createIndex({name: 1});
 db.restaurants.createIndex({ borough: 1}, {name: "filtered_by_borough_and_cuisine", partialFilterExpression: {borough: "Bronx", cuisine:{$in: ["American", "Chinese"]} } });
+db.restaurants.createIndex({borough: 1}, {name: "filtered_by_borough", partialFilterExpression: {borough: {$in: ["Staten Island", "Queens", "Bronx", "Brooklyn"] } } });
